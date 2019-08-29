088cfd453d052efef0791d91ad4326438d6a6b0b
Matt Jones <matthew.d.jones1990@gmail.com>
2019-08-28 18:53:58 -0700

    Add C# wrapper project to silently run node (no console window).

---
+++ RunNodeSilent/RunNodeSilent/RunNodeSilent.cs
@@ -0,0 +1,28 @@
+﻿/**
+ * File: RunNodeSilent.cs
+ * Author: Matt Jones
+ * Date: 28.08.2019
+ * Desc: A wrapper for Node (JavaScript interpreter) that silently runs scripts (no console window).
+ */
+
+using System;
+using System.Diagnostics;
+
+namespace RunNodeSilent {
+    static class RunNodeSilent {
+        [STAThread]
+        static void Main(String[] args) {
+            // The args must contain the name of the script to run.
+            if (args.Length < 1) return;
+
+            ProcessStartInfo info = new ProcessStartInfo();
+            info.FileName = "node.exe";
+            info.Arguments = args[0];
+            info.WindowStyle = ProcessWindowStyle.Hidden;
+
+            Process nodeProcess = new Process();
+            nodeProcess.StartInfo = info;
+            nodeProcess.Start();
+        }
+    }
+}
