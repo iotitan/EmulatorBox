36342f4fa53a7cd6b6d3da420a7872e8886f8c6f
Matt Jones <matthew.d.jones1990@gmail.com>
2019-08-28 18:55:25 -0700

    Update KillEmulators.js to run asynchronously and with less overhead.

--- scripts/KillEmulators.js
+++ scripts/KillEmulators.js
@@ -15,7 +15,7 @@
  * Run: node ./KillEmulators.js
  */
 
-const {execSync} = require('child_process');
+const {exec} = require('child_process');
 const process = require('process');
 
 const emuProcNames = [
@@ -30,27 +30,38 @@ const emuProcNames = [
 ];
 
 /**
- * Kill an emulator process by its process name (Windows specific).
+ * Asynchronously kill an emulator process by its process name (Windows specific). This first finds
+ * the PID of the named process and calles process.kill(...).
  * @param {string} processName The name of the process to attempt to kill.
  */
 function killEmulatorProcess(processName) {
     // List tasks without column headers (/nh), in CSV format (/fo "csv"), with the specified
     // image name (/fi "IMAGENAME eq emu.exe").
-    let out = execSync("tasklist /nh /fo \"csv\" /fi \"IMAGENAME eq "
-            + processName + "\"").toString();
-    out = out.split(",");
+    exec("tasklist /nh /fo \"csv\" /fi \"IMAGENAME eq " + processName + "\"",
+            (error, stdout, stderr)=> {
+                let out = stdout.split(",");
 
-    // The first item in the csv is always the process name. If the process wasn't found, do
-    // nothing. Remove the quotes from the output and check the name if it's there.
-    if (out.length < 2 || (out[0].substr(1, out[0].length - 2) != processName)) return;
+                // The first item in the csv is always the process name. If the process wasn't
+                // found, do nothing. Remove the quotes from the output and check the name if it's
+                // there.
+                if (out.length < 2 || (out[0].substr(1, out[0].length - 2) != processName)) return;
 
-    // Remove ther quotes from the output of index 1 which should always have the PID.
-    let pid = out[1].substr(1, out[1].length - 2);
+                // Remove ther quotes from the output of index 1 which should always have the PID.
+                let pid = out[1].substr(1, out[1].length - 2);
 
-    process.kill(pid);
+                process.kill(pid);
+            });
+}
+
+/**
+ * Asynchronously kill an emulator process by its process name (Windows specific).
+ * @param {string} processName The name of the process to attempt to kill.
+ */
+function killEmulatorProcessNoPID(processName) {
+    exec("TASKKILL /F /FI \"IMAGENAME eq " + processName + "\"");
 }
 
 // Loop over known emulators and kill their processes.
 for (let i = 0; i < emuProcNames.length; i++) {
-    killEmulatorProcess(emuProcNames[i]);
+    killEmulatorProcessNoPID(emuProcNames[i]);
 }
