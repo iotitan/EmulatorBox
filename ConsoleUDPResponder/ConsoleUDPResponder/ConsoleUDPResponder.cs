e71d22d7ff809d2114824c6227f191be066996a3
Matt Jones <matthew.d.jones1990@gmail.com>
2019-09-03 21:48:51 -0700

    Add c# project for UDP responder for phone app
    The code so far will respond to any broadcast with the "magic prefix"
    followed by the machine name.

---
+++ ConsoleUDPResponder/ConsoleUDPResponder/ConsoleUDPResponder.cs
@@ -0,0 +1,41 @@
+﻿/**
+ * File: ConsoleUDPResponder.cs
+ * Author: Matt Jones
+ * Date: 2019.09.03
+ * Desc: Respond to UDP connections with information about the console (name, IP, etc.).
+ */
+
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ConsoleUDPResponder {
+    static class ConsoleUDPResponder {
+
+        private static readonly String MAGIC_PREFIX = "!ConsoleInfo:";
+        private static readonly String SEPARATOR = "|";
+
+        [STAThread]
+        static void Main(String[] args) {
+            int port = 19002;
+            if (args.Length > 0) port = Int32.Parse(args[0]);
+
+            UdpClient socket = new UdpClient(port);
+
+            // This object will be populated with the sender's info when a connection is established.
+            IPEndPoint senderInfo = new IPEndPoint(IPAddress.Any, 0);
+
+            // Keep listening for incoming info requests until the machine turns off.
+            while (true) {
+                String broadcastData = Encoding.UTF8.GetString(socket.Receive(ref senderInfo));
+
+                byte[] responseBytes = Encoding.UTF8.GetBytes(
+                        MAGIC_PREFIX + SEPARATOR + Environment.MachineName + SEPARATOR);
+
+                socket.Send(responseBytes, responseBytes.Length, new IPEndPoint(senderInfo.Address, 19003));
+
+            }
+        }
+    }
+}
